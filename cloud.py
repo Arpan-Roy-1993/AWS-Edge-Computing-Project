0f962a229d3995ccb0db4e4ebd09570b784e4295
Arpan-Roy-1993 <43732420+Arpan-Roy-1993@users.noreply.github.com>
2020-04-01 20:20:53 -0700

    Add files via upload

--- cloud.py
+++ cloud.py
@@ -1,141 +1,132 @@
+import sys
 import boto3
-import os
-from os import listdir
 import time
-from threading import Thread
-import collections
+from botocore.exceptions import ClientError
+from ec2_metadata import ec2_metadata
+import os
+import subprocess
+import threading
 
-from os.path import isfile, join
+s3_results_bucket = "cc-object-detection-results"
 
+sqs = boto3.client('sqs')
+s3 = boto3.client('s3')
+ec2 = boto3.client('ec2')
 
+response = sqs.get_queue_url(QueueName='object_detection_requests_queue')
+object_detection_requests_queue_url = response['QueueUrl']
 
-ACCESS_KEY='ASIAX5QLJEL6XCXL6CEK'
-SECRET_KEY='n1MhR6oj3+7iDXAE1dLxiQSh+172l6ldCaKjXX97'
-SESSION_TOKEN='FwoGZXIvYXdzEJb//////////wEaDBuTxBf5+q5qop41giK/AZ4csvpqrHidSpWyroeyTThvE2pk487L8eIytfjyCt4QkuoyrR1IijXbAtVtKZoYiUY2uAaZK/CEA6KNbygH/vGINRPdHoV5JzMN2Du5XABTEjbhCDLFxooh7JhXxmjaH0MTYXmksE99G17ehfklUuBvLLvA4i9PPSEXgaCHMurO2mGoecr0yT7IgFemdaql0cjl3q7y8iKVvPN4sJl/rBr/CwNEaAmE2TEUTcVvsxXSwvwlH9UaWcs4jkivAb3cKOCM7/MFMi21MVQJ3YXWVGLzvqLmIfhnT4eTHvftvwukOAZuTHXNYMI45EUyWG/HZilRtgo='
+response = sqs.get_queue_url(QueueName='debugging_queue')
+debugging_queue_url = response['QueueUrl']
 
+class ProgressPercentage(object):
 
-listofvideos=[]
-filetoobject={}
-def pollbucketandrundarknet():
-#  time.sleep(5) 
-    global listofvideos
+    def __init__(self, client, bucket, filename):
+        self._filename = filename
+        self._size = client.head_object(Bucket=bucket, Key=filename).get('ContentLength')
+        self._seen_so_far = 0
+        self._lock = threading.Lock()
 
-    #os.system("Xvfb :1 & export DISPLAY=:1")
-    session = boto3.Session(
-        aws_access_key_id=ACCESS_KEY,
-        aws_secret_access_key=SECRET_KEY,
-        aws_session_token=SESSION_TOKEN,
+    def __call__(self, bytes_amount):
+        # To simplify, assume this is hooked up to a single filename
+        with self._lock:
+            self._seen_so_far += bytes_amount
+            percentage = (self._seen_so_far / self._size) * 100
+            sys.stdout.write(
+                "\r%s  %s / %s  (%.2f%%)" % (
+                    self._filename, self._seen_so_far, self._size,
+                    percentage))
+            sys.stdout.flush()
+
+def downloadFromS3(video_name):
+    s3_resource = boto3.resource('s3')
+    s3_resource.Bucket('videos-not-processed').download_file(video_name, video_name)
+    print("rahulsanjay", "Download done")
+
+def shutdown():
+    # Get the current instance's id
+    print("rahulsanjay","shutting down this instance ", str(ec2_metadata.instance_id))
+    try:
+        response = ec2.stop_instances(InstanceIds=[ec2_metadata.instance_id], DryRun=False)
+        print("rahulsanjay",str(response))
+    except ClientError as e:
+        print("rahulsanjay", e)
+
+def uploadToS3(videoname, results):
+    print("rahulsanjay","uploading the results to s3")
+    s3.put_object(Body = str(results), Bucket=s3_results_bucket, Key=videoname)
+
+no_gui_flag = False
+output = subprocess.check_output('ps')
+for line in output.decode('utf-8').splitlines():
+    if 'Xvfb' in line and 'Killed' not in line:
+        l = line.split()
+        no_gui_flag = True
+        pid = l[0]
+        break
+if not no_gui_flag:
+    print("enabling no gui")
+    os.system("Xvfb :1 &")
+else:
+    print("no gui already enabled")
+
+count = 0
+while True:
+    # Receive message from SQS queue
+    response = sqs.receive_message(
+        QueueUrl=object_detection_requests_queue_url,
+        AttributeNames=[
+            'SentTimestamp'
+        ],
+        MaxNumberOfMessages=1,
+        MessageAttributeNames=[
+            'All'
+        ],
+        VisibilityTimeout=0,
+        WaitTimeSeconds=5
     )
-    s3 = session.resource('s3')
-    bucket = s3.Bucket('videosnotprocessed')
-    # while True:
-    for obj in bucket.objects.all():
-        listofvideos.append(obj.key)
-        bucket.download_file(obj.key, '/home/ubuntu/darknet/videostobeprocessed/'+str(obj.key))
-    sendvideostosqs() 
-  '''  if  listofvideos:
+    if 'Messages' in response:
+        message = response['Messages'][0]
+        receipt_handle = message['ReceiptHandle']
+        body = message['Body']
 
-    	print("\n\nThe controller will process the video "+str(listofvideos[0]))
-    	print("\n\n")
-    	os.system("./darknet detector demo cfg/coco.data cfg/yolov3-tiny.cfg yolov3-tiny.weights /home/ubuntu/darknet/videostobeprocessed/"+str(listofvideos[0])+ " >/home/ubuntu/darknet/results/"+str(listofvideos[0]).replace('.',"")+".txt")
-	
-    	os.chdir("/home/ubuntu/darknet/results/")
-    	mypath="/home/ubuntu/darknet/results/"
-    	onlyfiles=[f for f in listdir(mypath) if isfile(join(mypath,f))]
-	#print(onlyfiles)
-    	print("\n\n\n")
-    	print("\n\n\n")
-    	for file in onlyfiles:
-        	listofobjects=set()
-        	f=open(file,'r')
-        	lines=f.readlines()
-        	for line in lines:
-     	      		if 'Object' in line or 'file' in line or 'video' in line or 'FPS' in line:
-                		continue
-            		if ':' in line:
-               			line=line.split(':')
-                		line += str("results")
-               	 		listofobjects.add(line[0])
-        	listofobjects=list(listofobjects)
-        	filetoobject[file]=listofobjects
-    	uploadresulttos3(filetoobject)
-    print("this will be executed\n")
-  #os.system("rm /home/ubuntu/darknet/videostobeprocessed/"+str(listofvideos[0]))
-'''
-def uploadresulttos3(filetoobject):
+        print("rahulsanjay", "got the video name ", str(body)," now going to download from s3")
 
-    client = boto3.client(
-        's3',
-        aws_access_key_id=ACCESS_KEY,
-        aws_secret_access_key=SECRET_KEY,
-        aws_session_token=SESSION_TOKEN
-    )
-    #print(client)
-    print("Uploading to s3..\n")
-    for key,value in filetoobject.items():
-        value=' '.join(value)
-        print("objects are:\n")
-        print(value)
-        client.put_object(Body=value,Bucket='raspberry',Key=key)
-    filetoobject={}
-    print("completed")
+        # Delete received message from queue -> should we only delete once download is successful ??
+        sqs.delete_message(
+            QueueUrl=object_detection_requests_queue_url,
+            ReceiptHandle=receipt_handle
+        )
 
-def sendvideostosqs():
-    global listofvideos
-    sqs = boto3.client(
-        'sqs',
-        aws_access_key_id=ACCESS_KEY,
-        aws_secret_access_key=SECRET_KEY,
-        aws_session_token=SESSION_TOKEN,
-        region_name='us-east-1'
-    )
+        os.chdir("/home/ubuntu/darknet/")
+        downloadFromS3(body)
+        output = subprocess.check_output(["./darknet", "detector", "demo", "cfg/coco.data", "cfg/yolov3-tiny.cfg",
+                                          "yolov3-tiny.weights", body])
 
-    # print(sqs)
+        s = set()
+        for line in output.decode('utf-8').splitlines():
+            if "%" in line:
+                l = line.split(':')
+                s.add(l[0])
 
-    queue_url = 'https://sqs.us-east-1.amazonaws.com/544410772221/videos_still_not_processed'
-    # Send message to SQS queue
-    #listofvideos.remove(listofvideos[0])
-    while listofvideos:
+        uploadToS3(body, list(s))
+
+        print("rahulsanjay", "upload done, sending message to debugging queue")
         response = sqs.send_message(
-        QueueUrl=queue_url,
-        DelaySeconds=10,
-        MessageAttributes={
-
-                 'Video_Name': {
-                 'DataType': 'String',
-                 'StringValue': str(listofvideos[0])
-
-                        }
-
-                },
-                MessageBody=(
-                        'Videos still to be processed '
-
-                )
-                )
-        print("sending video to SQS:"+str(listofvideos[0]))
-	os.system("rm /home/ubuntu/darknet/videostobeprocessed/"+str(listofvideos[0]))
-        listofvideos.remove(listofvideos[0])
-
-t1= Thread(target=pollbucketandrundarknet,args=[])
-t1.start()
-'''while True:
-	if len(listofvideos)<=0:
-		print("\n\nno videos in queue...\n\n")
-		time.sleep(5)
-		continue
-	else:
-		time.sleep(3)
-		while len(listofvideos)>0:
-    			if t1.isAlive():
-#        			print(" sending rest of the videos to SQS to be processed by other instances....\n")
-        			t2=Thread(target=sendvideostosqs())
-        			t2.start()
-    			else:
-        			print("\nThe controller  will execute this video\n")
-        			t1=Thread(target=pollbucketandrundarknet(),args=[])
-        			t1.start()
-
-'''
-
-
+            QueueUrl=debugging_queue_url,
+            DelaySeconds=0,
+            MessageAttributes={
+                'key': {
+                    'DataType': 'String',
+                    'StringValue': 'rahul_sanjay'
+                }
+            },
+            MessageBody=(
+                ec2_metadata.instance_id + ":" + body + ":" + str(s)
+            )
+        )
+    else:
+        print("rahulsanjay","No Messages in the queue")
+        count += 1
+        if count == 1:
+            shutdown()
